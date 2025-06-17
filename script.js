05e44ec220d130bd61e681298b5c3b3f27d87627
Jose Manuel Cisneros <119477560+Lstrappare@users.noreply.github.com>
2025-06-17 09:47:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,60 +5,52 @@ document.getElementById('iniciar').addEventListener('click', function () {
     let intercambio = 1;
     let pausar = false;
 
-    async function trasposicionParesNones(arr) {
-        let intercambioNones, intercambioPares;
+async function trasposicionParesNones(arr) {
+    let intercambioNones, intercambioPares;
 
-        mostrarEstadoInicial(arr);
+    mostrarEstadoInicial(arr);
 
-        do {
-            intercambioNones = false;
-            intercambioPares = false;
+    do {
+        intercambioNones = false;
+        intercambioPares = false;
 
-            // Etapa 1: Comparar nones y hacer intercambios
-            for (let i = 0; i < arr.length - 1; i += 2) {
-                if (arr[i] > arr[i + 1]) {
-                    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-                    intercambioNones = true;
-                }
+        // Comparar nones
+        for (let i = 0; i < arr.length - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                intercambioNones = true;
             }
-            await mostrarPaso(intercambio, 'Nones', arr);
+        }
+        await mostrarPaso(intercambio, 'Nones', arr);
 
-            // Etapa 2: Comparar pares y hacer intercambios
-            for (let i = 1; i < arr.length - 1; i += 2) {
-                if (arr[i] > arr[i + 1]) {
-                    [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-                    intercambioPares = true;
-                }
+        // Comparar pares
+        for (let i = 1; i < arr.length - 1; i += 2) {
+            if (arr[i] > arr[i + 1]) {
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                intercambioPares = true;
             }
-            await mostrarPaso(intercambio, 'Pares', arr);
+        }
+        await mostrarPaso(intercambio, 'Pares', arr);
 
-            intercambio++;
+        intercambio++;
 
-            while (pausar) {
-                await new Promise(resolve => setTimeout(resolve, 100));
-            }
-        } while (intercambioNones || intercambioPares);
-    }
+        while (pausar) {
+            await new Promise(resolve => setTimeout(resolve, 100));
+        }
+    } while (intercambioNones || intercambioPares);
+}
 
-    function mostrarEstadoInicial(arr) {
-        document.getElementById('estadoInicial').innerText = `Estado inicial del arreglo: [${arr.join(', ')}]`;
-    }
-
-    function mostrarPaso(iteracion, tipo, arr) {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                // Columna
-                let columna = document.createElement('div');
-                columna.classList.add('columna');
-                columna.innerText = `Intercambio ${intercambio} (${tipo}): [${arr.join(', ')}]`;
-
-                // Agregar la columna al contenedor
-                document.getElementById('columnas').appendChild(columna);
-
-                resolve();
-            }, 1000);
-        });
-    }
+function mostrarPaso(iteracion, tipo, arr) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            let columna = document.createElement('div');
+            columna.classList.add('columna');
+            columna.innerText = `Intercambio ${iteracion} (${tipo}): [${arr.join(', ')}]`;
+            document.getElementById('columnas').appendChild(columna);
+            resolve();
+        }, 500); // puedes ajustar velocidad
+    });
+}
     
     document.getElementById('iniciar').addEventListener('click', function () {
         document.getElementById('columnas').innerHTML = '';
